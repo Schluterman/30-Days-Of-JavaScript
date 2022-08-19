2ee790a75535412df21dc6054db9a4fff616edde
Schluterman <60680674+Schluterman@users.noreply.github.com>
2022-08-19 11:36:02 -0500

    started day25

--- 25_Day_World_countries_data_visualization_1/25_day_starter/scripts/main.js
+++ 25_Day_World_countries_data_visualization_1/25_day_starter/scripts/main.js
@@ -1,2 +1,11 @@
+import {countries_data} from "./data/countries_data.js";
 console.log(countries)
 alert('Open the console and check if the countries has been loaded')
+function topTenPop(){
+    const populationOfCountries = countries_data.population
+    
+}
+function topTenLang(){
+    const mostSpokenLang =countries_data['languages']
+
+}
