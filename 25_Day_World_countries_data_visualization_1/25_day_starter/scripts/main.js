144a89abfe4e6be00fa5c567f63f28ce8ff66880
Schluterman <60680674+Schluterman@users.noreply.github.com>
2022-08-20 07:18:14 -0500

    near complete day25

--- 25_Day_World_countries_data_visualization_1/25_day_starter/scripts/main.js
+++ 25_Day_World_countries_data_visualization_1/25_day_starter/scripts/main.js
@@ -1,11 +1,38 @@
-import {countries_data} from "./countries_data.js";
-console.log(countries)
+console.log(countries_data)
 alert('Open the console and check if the countries has been loaded')
-function topTenPop(){
-    const populationOfCountries = countries_data.population
-    
-}
-function topTenLang(){
-    const mostSpokenLang =countries_data['languages']
 
+function topTenPop(){
+    const pop = countries_data.sort((a, b) => b.population - a.population).slice(0, 10)
+    data.datasets[0].data = pop.map(item => item.name)
+    data.datasets[1].data = pop.map(item => item.population)
+    updateChart();
+}
+
+function topTenLang(){
+    let countries = {}
+
+    countries_data.forEach(({languages}) => {
+        languages.forEach(language => {
+            if (Object.keys(countries).includes(language)) {
+                countries[language] += 1
+            } else {
+                countries[language] = 1
+            }
+        })
+    })
+    let sortable = [];
+    for (let country in countries) {
+        sortable.push([country, countries[country]]);
+    }
+
+
+    sortable.sort(function(a, b) {
+        return b[1] - a[1];
+    });
+
+    sortable = sortable.slice(0,10)
+
+    data.datasets[0].data = sortable.map(item => item[0])
+    data.datasets[1].data = sortable.map(item => item[1])
+    updateChart();
 }
